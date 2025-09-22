8daeec0d8411d377bc2540e2f201d789735f994a
Athirakrishn <athiraramakrishnan41020@gmail.com>
2025-09-22 22:25:26 +0530

    preloader completed

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Preloader() {
   return (
     <div>
-      <div className="d-flex justify-content-center align-items-center " style={{height:"100vh"}}>
+      <div className="flex justify-center items-center h-center" style={{height:"100vh"}}>
         <img src="https://i.pinimg.com/originals/e1/59/25/e15925c931a81678a3c2e0c0a40db781.gif" alt="" />
       </div>
     </div>
